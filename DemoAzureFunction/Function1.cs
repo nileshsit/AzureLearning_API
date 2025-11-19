7b9ea5162e7189e9a310842dbe02cf3a98f90653
PGhanshyam <89398372+PGhanshyam@users.noreply.github.com>
2025-11-19 15:54:41 +0530

    undo the changes

--- DemoAzureFunction/Function1.cs
+++ DemoAzureFunction/Function1.cs
@@ -17,21 +17,21 @@ namespace DemoAzureFunction
             _logger = loggerFactory.CreateLogger<Function1>();
         }
 
-        //[Function("Function1")]
-        //public HttpResponseData run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        //{
-        //    _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-        //    var response = req.CreateResponse(HttpStatusCode.OK);
-        //    response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-        //    response.WriteString("Welcome to Azure Functions!");
-
-        //    return response;
-        //}
-
         [Function("Function1")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public HttpResponseData run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+
+        [Function("Function2")]
+        public async Task<HttpResponseData> Function2Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("Function2 executed.");
 
